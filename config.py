eb7a9b6c625e58187c6ea87cc2117a043caf7304
KarthikTamil7 <108778191+KarthikTamil7@users.noreply.github.com>
2023-01-19 19:05:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # PLEASE EDIT THIS FILE IF YOU DEPLOYING THIS BOT ON VPS. ELSE *DON'T* TOUCH THIS FILE.
 
 # Please edit 'CONFIG = False' to 'CONFIG = True'
-CONFIG = False
+CONFIG = True
 
 # Add your APP ID from my.telegram.org
 API_ID = 123456
